949aaec2b79809980d2204fc5686c5a69ef8f54e
DavidBarta <davidbarta0505@gmail.com>
2024-03-25 12:31:38 +0100

    Level 2

---
+++ plugins/appuser/user/controllers/users/_list_toolbar.php
@@ -0,0 +1,22 @@
+<!--<div data-control="toolbar loader-container">-->
+<!--    <a-->
+<!--        href="--><?php //= Backend::url('appuser/user/users/create') ?><!--"-->
+<!--        class="btn btn-primary">-->
+<!--        <i class="icon-plus"></i>-->
+<!--        --><?php //= __("New :name", ['name' => 'User']) ?>
+<!--    </a>-->
+
+<!--    <div class="toolbar-divider"></div>-->
+<!---->
+<!--    <button-->
+<!--        class="btn btn-secondary"-->
+<!--        data-request="onDelete"-->
+<!--        data-request-message="--><?php //= __("Deleting...") ?><!--"-->
+<!--        data-request-confirm="--><?php //= __("Are you sure?") ?><!--"-->
+<!--        data-list-checked-trigger-->
+<!--        data-list-checked-request-->
+<!--        disabled>-->
+<!--        <i class="icon-delete"></i>-->
+<!--        --><?php //= __("Delete") ?>
+<!--    </button>-->
+<!--</div>-->
---
+++ plugins/appuser/user/controllers/users/create.php
@@ -0,0 +1,61 @@
+<?php //Block::put('breadcrumb') ?>
+<!--    <ol class="breadcrumb">-->
+<!--        <li class="breadcrumb-item"><a href="--><?php //= Backend::url('appuser/user/users') ?><!--">Users</a></li>-->
+<!--        <li class="breadcrumb-item active" aria-current="page">--><?php //= e($this->pageTitle) ?><!--</li>-->
+<!--    </ol>-->
+<?php //Block::endPut() ?>
+<!---->
+<?php //if (!$this->fatalError): ?>
+<!---->
+<!--    --><?php //= Form::open(['class' => 'd-flex flex-column h-100']) ?>
+<!---->
+<!--        <div class="flex-grow-1">-->
+<!--            --><?php //= $this->formRender() ?>
+<!--        </div>-->
+<!---->
+<!--        <div class="form-buttons">-->
+<!--            <div data-control="loader-container">-->
+<!--                <button-->
+<!--                    type="submit"-->
+<!--                    data-request="onSave"-->
+<!--                    data-request-message="--><?php //= __("Creating :name...", ['name' => $formRecordName]) ?><!--"-->
+<!--                    data-hotkey="ctrl+s, cmd+s"-->
+<!--                    class="btn btn-primary">-->
+<!--                    --><?php //= __("Create") ?>
+<!--                </button>-->
+<!--                <button-->
+<!--                    type="button"-->
+<!--                    data-request="onSave"-->
+<!--                    data-request-data="{ close: 1 }"-->
+<!--                    data-request-message="--><?php //= __("Creating :name...", ['name' => $formRecordName]) ?><!--"-->
+<!--                    data-hotkey="ctrl+enter, cmd+enter"-->
+<!--                    class="btn btn-default">-->
+<!--                    --><?php //= __("Create & Close") ?>
+<!--                </button>-->
+<!--                <span class="btn-text">-->
+<!--                    <span class="button-separator">--><?php //= __("or") ?><!--</span>-->
+<!--                    <a-->
+<!--                        href="--><?php //= Backend::url('appuser/user/users') ?><!--"-->
+<!--                        class="btn btn-link p-0">-->
+<!--                        --><?php //= __("Cancel") ?>
+<!--                    </a>-->
+<!--                </span>-->
+<!--            </div>-->
+<!--        </div>-->
+<!---->
+<!--    --><?php //= Form::close() ?>
+<!---->
+<?php //else: ?>
+<!---->
+<!--    <p class="flash-message static error">-->
+<!--        --><?php //= e($this->fatalError) ?>
+<!--    </p>-->
+<!--    <p>-->
+<!--        <a-->
+<!--            href="--><?php //= Backend::url('appuser/user/users') ?><!--"-->
+<!--            class="btn btn-default">-->
+<!--            --><?php //= __("Return to List") ?>
+<!--        </a>-->
+<!--    </p>-->
+<!---->
+<?php //endif ?>
